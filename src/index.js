ffaa051c2fd74216cd3e3c2cd2d854c1ee1c925e
Roni <76941552+ronieo@users.noreply.github.com>
2021-08-26 19:20:36 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import SimpleHabit from './components/simpleHabit';
 
 ReactDOM.render( // React.StrictMode: Component들을 한번 더 랜더링 해주고 잘못 된 것은 없는지 체크하는 역할(실제 배포 시 보이지 않음)
   <React.StrictMode>
-    <SimpleHabit/>
-    {/*<App />*/}
+   {/*<SimpleHabit/>*/}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
